63f68bb9d603a156582f21bbedcc945e8e604e28
abdulazizv <abdulazizheir@gmail.com>
2023-02-22 23:03:44 +0500

    extra.vue added

---
+++ src/views/Extra.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-blue-500 p-20">
+        <h1 class="text-white">EXTRA </h1>
+
+  
+    </div>
+</template>
+<script>
+export default {
+    name: "Extra",
+}
+</script>
+<style lang="scss"></style>
