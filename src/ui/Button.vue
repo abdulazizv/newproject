b54737eec84f00bea4d3cd4e981699f01614c648
abdulazizv <abdulazizheir@gmail.com>
2023-02-22 23:08:02 +0500

    changed router

--- src/ui/Button.vue
+++ src/ui/Button.vue
@@ -1,17 +1,15 @@
 <template>
-
-    <button :type="btnType" class="py-2 px-4 focus:ring-sky-500  focus:ring-2 mx-auto block">{{ textContent }}</button>
-
+    <button :type="btnType" class="py-2 px-4   focus:ring-2 focus:ring-sky-500 focus:outline-none  ">{{ textContent
+    }}</button>
 </template>
+
 <script>
 export default {
-name:'Button',
-props:{
-    btnType:String,
-    textContent:String
-}   
+    name: "Button",
+    props: {
+        btnType: String,
+        textContent: String,
+    }
 }
 </script>
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- src/ui/Input.vue
+++ src/ui/Input.vue
@@ -1,20 +1,20 @@
 <template>
-    <label :for="labelId" class="w-[300px] block my-3 mx-auto">
+    <label :for="labelId" class="w-1/2 block my-3 mx-auto">
         <p class="text-indigo-500 my-2" v-if="placeholder">{{ placeholder }}</p>
-        <input type="text" :id="labelId"  class="p-3 w-full ring-2 focus:ring-cyan-700 focus:outline-none placeholder:text-slate-700" :placeholder="placeholder" :value="value">
+        <input :type="inputType" :id="labelId"
+            class="p-3 w-full ring-2 focus:ring-cyan-700 focus:outline-none placeholder:text-slate-300"
+            :placeholder="placeholder" :value="value" />
     </label>
 </template>
 <script>
 export default {
-    name:'Input',
+    name: "Input",
     props: {
         labelId: String,
         placeholder: String,
-        inputType : String,
-        value:String
-    }
-}
+        inputType: String,
+        value: String
+    },
+};
 </script>
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
